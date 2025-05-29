c96be5a132192dfaa4bc45588324d4ef1ff5ecee
Tomas <bekjon_98@mail.ru>
2025-05-29 22:21:14 +0300

    update env variable

--- src/network/axios.js
+++ src/network/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: "http://localhost:5205/api/v1/pastes", //dev env
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1/pastes`, //dev env
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
